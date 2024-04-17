d484b9f0a8f74c7ea34715fd5ffc746ed1859c43
newestzdn <127198882+newestzdn@users.noreply.github.com>
2024-04-17 15:25:57 +0700

    Update pixeldrain_upload.sh

--- pixeldrain_upload.sh
+++ pixeldrain_upload.sh
@@ -3,4 +3,14 @@
 for file in *.zip;
 do curl -T "$file" https://pixeldrain.com/api/file/; done >> anu1.txt
 
+function push() {
+LINKROM=$(echo *.txt)
+        curl -F document=@$LINKROM "https://api.telegram.org/bot$TOKEN_BOT_TELE/sendDocument" \
+        -F chat_id="$ID_CHAT" \
+        -F "disable_web_page_preview=true" \
+        -F "parse_mode=html" \
+        -F caption="Build Done!." 
+}
+
+push
 
